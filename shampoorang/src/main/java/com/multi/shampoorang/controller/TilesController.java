5844f79e3d35a55ed4ab3550bbe5da2022874c66
cubodado <dalkang000@gmail.com>
2021-06-02 09:59:25 +0900

    feat: 홈화면 경로 생성

--- shampoorang/src/main/java/com/multi/shampoorang/controller/TilesController.java
+++ shampoorang/src/main/java/com/multi/shampoorang/controller/TilesController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TilesController {	
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "index";
+    }
+    
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "index";
