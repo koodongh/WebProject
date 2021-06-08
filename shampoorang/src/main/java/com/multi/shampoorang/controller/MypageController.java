2991a7f5780d41218b470634bf261145240e4a8a
cubodado <dalkang000@gmail.com>
2021-06-08 16:28:32 +0900

    feat: 피해야 하는 성분 페이지 생성

--- shampoorang/src/main/java/com/multi/shampoorang/controller/MypageController.java
+++ shampoorang/src/main/java/com/multi/shampoorang/controller/MypageController.java
@@ -1,26 +1,40 @@
 package com.multi.shampoorang.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.multi.shampoorang.model.ProductVO;
+import com.multi.shampoorang.service.ProductService;
 
 @Controller
 public class MypageController {
-	@RequestMapping("mypage/main")
+	@Autowired
+	ProductService service;
+	
+	@RequestMapping("/mypage/main")
 	public String mypage() {
 		return "mypage/main";
 	}
 	
-	@RequestMapping("mypage/like")
+	@RequestMapping("/mypage/like")
 	public String mypageLike() {
 		return "mypage/likeIngd";
 	}
 	
-	@RequestMapping("mypage/dislike")
-	public String mypageDislike() {
-		return "mypage/dislikeIngd";
+	@RequestMapping("/mypage/dislike")
+	public String viewProductList(Model model) {		
+		
+		ArrayList<ProductVO> ingdList = service.ingdList();
+		model.addAttribute("ingdList", ingdList);
+		 
+		return "mypage/dislikeIngd"; 
 	}
 	
-	@RequestMapping("mypage/resultList")
+	@RequestMapping("/mypage/resultList")
 	public String mypageResultList() {
 		return "mypage/resultList";
 	}
--- shampoorang/src/main/webapp/WEB-INF/views/jsp/view/mypage/dislikeIngd.jsp
+++ shampoorang/src/main/webapp/WEB-INF/views/jsp/view/mypage/dislikeIngd.jsp
@@ -1,11 +1,27 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<link href="/css/mypage/dislikeIngd.css" rel="stylesheet">
 	</head>
 	<body>
-		<h1>"disklikeIngd"</h1>
+		<div class="container">
+			<h3>샴푸 성분리스트</h3>
+			<form method="POST" action="">
+				<c:forEach items="${ingdList}" var="ingd">
+					<div class="form-check mt-2 mb-2">
+	  					<input class="form-check-input" type="checkbox" value="${ ingd.ingd_name }" id="flexCheckDefault">
+	  					<label class="form-check-label" for="flexCheckDefault">
+	    					${ ingd.ingd_name }
+	  					</label>
+					</div>   		
+				</c:forEach>
+				<input type="submit" value="저장">
+			</form>
+		</div>
 	</body>
 </html>
