f5a651378df2c165dd92808fe8196058e7b1d72b
koodongh <paul500089@gmail.com>
2021-06-15 23:27:59 +0900

    refactor: joinVO, OCRJoinVO, 최근 push jsp 파일 업로드

---
+++ shampoorang/src/main/java/com/multi/shampoorang/model/OcrJoinVO.java
@@ -0,0 +1,54 @@
+package com.multi.shampoorang.model;
+
+public class OcrJoinVO {
+	private String result_id;
+	private String member_id;
+	private String ingd_name;
+	private String ewg;
+	private int hazzard;
+	private String description;
+	private int ewg_encode;
+	public String getResult_id() {
+		return result_id;
+	}
+	public void setResult_id(String result_id) {
+		this.result_id = result_id;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getIngd_name() {
+		return ingd_name;
+	}
+	public void setIngd_name(String ingd_name) {
+		this.ingd_name = ingd_name;
+	}
+	public String getEwg() {
+		return ewg;
+	}
+	public void setEwg(String ewg) {
+		this.ewg = ewg;
+	}
+	public int getHazzard() {
+		return hazzard;
+	}
+	public void setHazzard(int hazzard) {
+		this.hazzard = hazzard;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getEwg_encode() {
+		return ewg_encode;
+	}
+	public void setEwg_encode(int ewg_encode) {
+		this.ewg_encode = ewg_encode;
+	}
+	
+}
---
+++ shampoorang/src/main/webapp/WEB-INF/views/jsp/view/mypage/dislikes_save.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Insert title here</title>
+	</head>
+	<body>
+			
+		<form method="POST" action="/dislikes_save">
+			
+			<input type="checkbox"   name="ingd" value="시트릭애씨드">웹 시트릭애씨드 <br>
+                         <input type="checkbox"   name="ingd" value="알루미늄스타치옥테닐석시네이트">알루미늄스타치옥테닐석시네이트<br>
+                         <input type="checkbox"  name="ingd" value="정제수">정제수<br>
+                    <input type="submit" value="저장" >	
+			</form>
+	</body>
+</html>
