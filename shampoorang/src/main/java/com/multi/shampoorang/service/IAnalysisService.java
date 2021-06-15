f057b6fcab32360dd937fb09609524a9efd58ab3
koodongh <paul500089@gmail.com>
2021-06-15 23:17:24 +0900

    chore: AnalysisServiceimpl 코드수정

--- shampoorang/src/main/java/com/multi/shampoorang/service/IAnalysisService.java
+++ shampoorang/src/main/java/com/multi/shampoorang/service/IAnalysisService.java
@@ -3,6 +3,8 @@ package com.multi.shampoorang.service;
 import java.util.ArrayList;
 
 import com.multi.shampoorang.model.AnalysisVO;
+import com.multi.shampoorang.model.JoinVO;
+import com.multi.shampoorang.model.OcrJoinVO;
 
 public interface IAnalysisService {
 
@@ -10,6 +12,8 @@ public interface IAnalysisService {
 	ArrayList<AnalysisVO> detailResult(String result_id);
 	ArrayList<AnalysisVO> memberAnalysisList(String member_id);
 	void Ocr(AnalysisVO ocr);
-
-
+	ArrayList<OcrJoinVO> ocrJoinList(String sid);
+	/*
+	 * ArrayList<JoinVO> joinList();
+	 */
 }
