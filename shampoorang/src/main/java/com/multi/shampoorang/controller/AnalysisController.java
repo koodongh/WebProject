fb7f9c7a42b6f6c1784f7433745a3f51247a8d37
koodongh <paul500089@gmail.com>
2021-06-15 23:11:36 +0900

    refactor:컨트롤로 ocr_join_list 코드 추가

--- shampoorang/src/main/java/com/multi/shampoorang/controller/AnalysisController.java
+++ shampoorang/src/main/java/com/multi/shampoorang/controller/AnalysisController.java
@@ -1,4 +1,4 @@
-/*아직 수정 필요합니당*/
+
 package com.multi.shampoorang.controller;
 
 import java.util.ArrayList;
@@ -12,36 +12,73 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.multi.shampoorang.model.AnalysisVO;
+import com.multi.shampoorang.model.JoinVO;
+import com.multi.shampoorang.model.OcrJoinVO;
 import com.multi.shampoorang.service.AnalysisService;
+import com.multi.shampoorang.service.ProductService;
 
 @Controller
 public class AnalysisController {
-   
+
 	@Autowired
 	AnalysisService service;
-	
-    @RequestMapping("/result/analysisView")
-    public String viewAnalysisList(Model model) {
 
-    ArrayList<AnalysisVO> analysisList = service.analysisList();
-	model.addAttribute("analysisList", analysisList);
-		 
-	return "/result/analysisView"; 
+	@Autowired
+	ProductService productService;
+
+	@RequestMapping("/result/analysisView")
+	public String viewAnalysisList(Model model) {
+
+		ArrayList<AnalysisVO> analysisList = service.analysisList();
+		model.addAttribute("analysisList", analysisList);
+
+		return "/result/analysisView";
 	}
-    
-    @RequestMapping("/result/ocr")
+
+	@RequestMapping("/result/ocr")
 	public String Ocr(AnalysisVO ocr, HttpSession session) {
 
 		((AnalysisService) service).Ocr(ocr);
+		return "redirect:/ocr_join_list";
+	}
+
+	@RequestMapping("/ocr_join_list")
+	public String ocr_join_list(Model model, HttpSession session) {
+
+		// sid 가져오기
+		String sid = (String) session.getAttribute("sid");
+		System.out.println("sid : " + sid);
+
+		ArrayList<OcrJoinVO> ocrJoinList = service.ocrJoinList(sid);
+
+		
+		/*
+		 * for(int i=0; i < ocrJoinList.size(); i++) {
+		 * System.out.print(ocrJoinList.get(i).getResult_id());
+		 * System.out.print(ocrJoinList.get(i).getMember_id());
+		 * System.out.print(ocrJoinList.get(i).getIngd_name());
+		 * System.out.print(ocrJoinList.get(i).getEwg());
+		 * System.out.print(ocrJoinList.get(i).getHazzard());
+		 * System.out.print(ocrJoinList.get(i).getDescription());
+		 * System.out.print(ocrJoinList.get(i).getEwg_encode()); System.out.println(); }
+		 * 
+		 */
+
+		model.addAttribute("ocrJoinList", ocrJoinList);
 		return "redirect:/result/analysisView";
 	}
-    
+	/*
+	 * @RequestMapping("/join_list") public String join_list(Model model) {
+	 * ArrayList<JoinVO> joinList = service.joinList();
+	 * model.addAttribute("joinList", joinList); return "mypage/join_list"; }
+	 */
+
 	@RequestMapping("/mypage/resultList/{member_id}/{result_id}")
 	public String mypageDetailResult(@PathVariable String member_id, @PathVariable String result_id, Model model) {
-		
+
 		ArrayList<AnalysisVO> detailResult = service.detailResult(result_id);
 		model.addAttribute("detailResult", detailResult);
-		
+
 		return "/result/resultDetailView";
 	}
 }
